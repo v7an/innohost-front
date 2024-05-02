1022a34afe9ca7149b29d4819f15d6209bbaa404
Snowi <154461763+hellosnowi@users.noreply.github.com>
2024-05-02 13:00:03 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -3,9 +3,9 @@
 useSeoMeta({
   titleTemplate: 'innohost.pro',
   ogType: 'website',
-  ogImage: 'https://media.discordapp.net/attachments/1229880919184117820/1235541449936994314/discotools-xyz-icon2.png?ex=6634bf24&is=66336da4&hm=000c54f7e407c04b50b8464cd382a18235934ced5ccf36947e72ede062bd3945&=&format=webp&quality=lossless',
-  twitterImage: 'https://media.discordapp.net/attachments/1229880919184117820/1235541449936994314/discotools-xyz-icon2.png?ex=6634bf24&is=66336da4&hm=000c54f7e407c04b50b8464cd382a18235934ced5ccf36947e72ede062bd3945&=&format=webp&quality=lossless',
-  twitterCard: 'summary_large_image'
+  //ogImage: 'https://media.discordapp.net/attachments/1229880919184117820/1235541449936994314/discotools-xyz-icon2.png?ex=6634bf24&is=66336da4&hm=000c54f7e407c04b50b8464cd382a18235934ced5ccf36947e72ede062bd3945&=&format=webp&quality=lossless',
+  //twitterImage: 'https://media.discordapp.net/attachments/1229880919184117820/1235541449936994314/discotools-xyz-icon2.png?ex=6634bf24&is=66336da4&hm=000c54f7e407c04b50b8464cd382a18235934ced5ccf36947e72ede062bd3945&=&format=webp&quality=lossless',
+  //twitterCard: 'summary_large_image'
 })
 </script>
 
