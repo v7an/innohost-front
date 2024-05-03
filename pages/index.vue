f8791c72d22416e6137a8c6c12bc5651e8760af7
Snowi <154461763+hellosnowi@users.noreply.github.com>
2024-05-03 18:20:26 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,7 @@
             <h1 data-aos="fade-right" class="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">Vous êtes entre de bonnes mains avec InnoHost.</h1>
             <p data-aos="fade-right" class="mt-6 text-lg leading-8 text-gray-300">InnoHost propose un hébergement gratuit idéal pour vos bots Discord, qu'ils soient développés en Node.js ou en Python. Notre plateforme offre un environnement sécurisé et fiable pour exécuter vos bots, avec des performances optimales garanties.</p>
             <div data-aos="fade-right" class="mt-10 flex items-center gap-x-6">
-              <a href="https://my.innohost.pro" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Commencer</a>
+              <a href="https://panel.innohost.pro" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Commencer</a>
               <a href="https://discord.gg/inno" class="text-sm font-semibold leading-6 text-white">Rejoignez notre discord <span aria-hidden="true">→</span></a>
             </div>
           </div>
@@ -93,10 +93,10 @@
           <div class="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20" style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)" />
         </div>
         <div class="mx-auto max-w-2xl text-center">
-          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Utilisez notre hébergeur aujourd'hui.</h2>
-          <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">Améliorer la sécurité et l'efficacité de votre serveur en intégrant dès aujourd'hui notre hébergeur de confiance, vous permettant ainsi de favoriser un environnement prospère et engagé.</p>
+          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Utilisez notre hébergeur dès aujourd'hui.</h2>
+          <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">Améliorer la sécurité et l'efficacité de votre projet en intégrant dès aujourd'hui notre hébergeur de confiance.</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
-            <a href="https://my.innohost.pro" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Commencer</a>
+            <a href="https://panel.innohost.pro" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Commencer</a>
             <a href="https://discord.gg/inno" class="text-sm font-semibold leading-6 text-white">Rejoignez notre discord <span aria-hidden="true">→</span></a>
           </div>
         </div>
