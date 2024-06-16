4958ec89ec3386b45ccf9f9375e8321d9febbc03
Snowi <154461763+hellosnowi@users.noreply.github.com>
2024-06-16 21:24:21 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -28,11 +28,11 @@
            <img data-aos="fade-down" class="h-11" src="../public/logo.png" alt="innohost logo" />
             <div class="mt-24 sm:mt-32 lg:mt-16">
             </div>
-            <h1 data-aos="fade-right" class="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">Vous êtes entre de bonnes mains avec InnoHost.</h1>
-            <p data-aos="fade-right" class="mt-6 text-lg leading-8 text-gray-300">InnoHost propose un hébergement gratuit idéal pour vos bots Discord, qu'ils soient développés en Node.js ou en Python. Notre plateforme offre un environnement sécurisé et fiable pour exécuter vos bots, avec des performances optimales garanties.</p>
+            <h1 data-aos="fade-right" class="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">You're in good hands with InnoHost.</h1>
+            <p data-aos="fade-right" class="mt-6 text-lg leading-8 text-gray-300">InnoHost offers ideal free hosting for your Discord bots, whether developed in Node.js or Python. Our platform offers a secure and reliable environment to run your bots, with guaranteed optimal performance.</p>
             <div data-aos="fade-right" class="mt-10 flex items-center gap-x-6">
               <a href="https://panel.innohost.pro" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Commencer</a>
-              <a href="https://discord.gg/inno" class="text-sm font-semibold leading-6 text-white">Rejoignez notre discord <span aria-hidden="true">→</span></a>
+              <a href="https://discord.gg/inno" class="text-sm font-semibold leading-6 text-white">Join our discord <span aria-hidden="true">→</span></a>
             </div>
           </div>
           
@@ -55,9 +55,9 @@
       <!-- Feature section -->
       <div class="mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8">
         <div class="mx-auto max-w-2xl text-center">
-          <h2 data-aos="fade-down" class="text-base font-semibold leading-7 text-indigo-400">Inno-Host</h2>
-          <p data-aos="fade-down" class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Votre serveur est entre de bonnes mains avec InnoHost.</p>
-          <p data-aos="fade-down" class="mt-6 text-lg leading-8 text-gray-300">Avec Inno-Host, libérez le plein potentiel de vos bots Discord, en toute fiabilité et gratuitement.</p>
+          <h2 data-aos="fade-down" class="text-base font-semibold leading-7 text-indigo-400">InnoHost</h2>
+          <p data-aos="fade-down" class="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">Your server is in good hands with InnoHost.</p>
+          <p data-aos="fade-down" class="mt-6 text-lg leading-8 text-gray-300">With InnoHost, unleash the full potential of your projects, reliably and free of charge..</p>
         </div>
         <div data-aos="fade-down" class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
           <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
@@ -93,11 +93,11 @@
           <div class="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20" style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)" />
         </div>
         <div class="mx-auto max-w-2xl text-center">
-          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Utilisez notre hébergeur dès aujourd'hui.</h2>
-          <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">Améliorer la sécurité et l'efficacité de votre projet en intégrant dès aujourd'hui notre hébergeur de confiance.</p>
+          <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Use our hosting service today.</h2>
+          <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">Improve the security and efficiency of your project by integrating our trusted hosting provider today.</p>
           <div class="mt-10 flex items-center justify-center gap-x-6">
             <a href="https://panel.innohost.pro" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">Commencer</a>
-            <a href="https://discord.gg/inno" class="text-sm font-semibold leading-6 text-white">Rejoignez notre discord <span aria-hidden="true">→</span></a>
+            <a href="https://discord.gg/inno" class="text-sm font-semibold leading-6 text-white">Join our discord <span aria-hidden="true">→</span></a>
           </div>
         </div>
       </div>
@@ -114,7 +114,7 @@
               <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
             </a>
           </div>
-          <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">&copy; 2024 Inno-Host, Inc. All rights reserved.</p>
+          <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">&copy; 2023- 2024 InnoHost, Inc. All rights reserved.</p>
         </div>
       </div>
     </footer>
@@ -135,45 +135,45 @@ import {
 import { BoltIcon, AdjustmentsHorizontalIcon, UsersIcon } from '@heroicons/vue/24/outline'
 const primaryFeatures = [
   {
-    name: 'Gratuité',
+    name: 'Free',
     description:
-      'InnoHost offre un hébergement gratuit pour vos bots Discord, ce qui vous permet de démarrer sans frais initiaux.',
+      'InnoHost offers free hosting for your projects, so you can get started with no up-front costs.',
     icon: BoltIcon,
   },
   {
-    name: 'Fiabilité',
+    name: 'Reliability',
     description:
-      'Notre infrastructure est conçue pour offrir une disponibilité maximale, garantissant que vos bots restent opérationnels et accessibles à tout moment.',
+      'Our infrastructure is designed for maximum availability, ensuring that your bots remain operational and accessible at all times.',
     icon: UsersIcon,
   },
   {
     name: 'Performance',
     description:
-      'Avec des serveurs optimisés et une connectivité rapide, vos bots bénéficieront de performances exceptionnelles, assurant une expérience fluide pour vos utilisateurs.',
+      'With optimized servers and fast connectivity, your bots will benefit from exceptional performance, ensuring a smooth experience for your users.',
     icon: AdjustmentsHorizontalIcon,
   },
 ]
 const secondaryFeatures = [
   {
-    name: 'Sécurité',
-    description: 'La sécurité est une priorité chez InnoHost. Nous utilisons des mesures de sécurité avancées pour protéger vos données et garantir la confidentialité de vos utilisateurs.',
+    name: 'Security',
+    description: 'Security is a priority at InnoHost. We use advanced security measures to protect your data and guarantee the confidentiality of your users.',
     icon: FingerPrintIcon,
   },
   {
-    name: 'Facilité d\'utilisationn',
-    description: 'Notre plateforme est conviviale et facile à utiliser, ce qui vous permet de déployer et de gérer vos bots Discord en quelques clics seulement.',
+    name: 'Ease of use',
+    description: 'Our user-friendly platform lets you deploy and manage your Discord bots in just a few clicks.',
     icon: Cog6ToothIcon,
   },
   {
     name: 'Latence',
-    description: 'Bénéficiez de temps de réponse ultra-rapides grâce à notre infrastructure optimisée, garantissant une latence minimale pour des performances ininterrompues.',
+    description: 'Benefit from ultra-fast response times thanks to our optimized infrastructure, guaranteeing minimal latency for uninterrupted performance.',
     icon: CloudArrowUpIcon,
   },
 ]
 const stats = [
-  { id: 1, name: 'Utilisateurs', value: 'N/A' },
+  { id: 1, name: 'Users', value: '80+' },
   { id: 2, name: 'Uptime', value: '99.9%' },
-  { id: 3, name: 'Serveurs enregistrés', value: 'N/A' },
+  { id: 3, name: 'Serveurs', value: '50+' },
 ]
 const footerNavigation = {
   solutions: [
